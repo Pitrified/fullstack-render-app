4bf392bf9976c2f292b72c41b1d4a2d1d2f223d6
Pitrified <nobilipietro@gmail.com>
2025-08-03 23:13:45 +0200

    Add log on database URL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,6 +1,7 @@
 import os
+
 from dotenv import load_dotenv
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -8,8 +9,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 
+print("Using DATABASE_URL:", DATABASE_URL)
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
 
 async def get_db():
     async with SessionLocal() as session:
